e29069b0dbd83ce604aa718d85792c2d13b4ba34
drei001 <dreivan.orprecio@sun-asterisk.com>
2021-10-29 15:14:23 +0800

    test(createdata.py): Update fake data creation function

--- backend/learning_app/management/commands/createdata.py
+++ backend/learning_app/management/commands/createdata.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from faker import Faker
 from authentication_app.models import Student
-from learning_app.models import Category
+from learning_app.models import Category, Question, Choice
 
 
 class Command(BaseCommand):
@@ -10,5 +10,13 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         fake = Faker()
 
-        for i in range(0, 30):
-            Category.objects.create(title=fake.word(), description=fake.sentence())
+        for i in range(0, 10):
+            obj = Category.objects.create(
+                title=fake.word(), description=fake.sentence()
+            )
+            for i in range(0, 3):
+                question = Question.objects.create(
+                    category=obj, description=fake.sentence()
+                )
+                for i in range(0, 3):
+                    Choice.objects.create(question=question, value=fake.word())
